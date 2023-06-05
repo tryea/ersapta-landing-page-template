8f50cb86538b90fff6d07cb681804848448bede4
aristoersapta@gmail.com <aristoersapta@gmail.com>
2023-06-05 15:59:14 +0700

    [main] add initial web display

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      {/* TODO: Header here */}
+      <header className="Header">
+        <h1>Welcome to My Website</h1>
+      </header>
+      {/* TODO: Welcome Section here */}
+      <section className="WelcomeSection">
+        <img src="https://source.unsplash.com/random/800x600" alt="Random from Unsplash" />
+        <p>Welcome to our website. We hope you find it informative and useful.</p>
+      </section>
+    </div>
   )
 }
 
